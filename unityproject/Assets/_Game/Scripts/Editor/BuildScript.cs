bdbe147a666559ed7520fb20c8920588221776cb
Carlos Macias <carlos.macias23@gmail.com>
2026-01-15 23:33:07 -0800

    add build script and other things

---
+++ unityproject/Assets/_Game/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class BuildScript
+{
+    [MenuItem("Build/Build WebGL")]
+    public static void BuildWebGL()
+    {
+        string buildPath = "Build/WebGL";
+        
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { EditorBuildSettings.scenes[0].path };
+        buildPlayerOptions.locationPathName = buildPath;
+        buildPlayerOptions.target = BuildTarget.WebGL;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        Debug.Log("Starting WebGL Build...");
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("<color=green>Build Succeeded!</color> Size: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.LogError("<color=red>Build Failed!</color>");
+        }
+    }
+}
